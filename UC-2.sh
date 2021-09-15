ed3c6b6c12d7d57e598a8023b35cd0fba8ccda59
shbodake <sayalibodake28@gmail.com>
2021-09-15 13:15:27 +0530

    UC2 code to compute a + b * c

---
+++ UC-2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Enter first number: "
+read a
+echo "Enter second number: "
+read b
+echo "Enter third number: "
+read c
+echo "Three numbers are: $a $b $c"
+echo "$a + $b * $c = $(( a + b * c ))"
