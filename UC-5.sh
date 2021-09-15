a51f2a3fefa65fd5053aa0f17b2d5ae3003817e6
shbodake <sayalibodake28@gmail.com>
2021-09-15 14:15:47 +0530

    UC5 code to compute a % b + c

---
+++ UC-5.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Enter first number: "
+read a
+echo "Enter second number: "
+read b
+echo "Enter third number: "
+read c
+echo "Three numbers are: $a $b $c"
+echo "$a + $b * $c = $(( a + b * c ))"
+echo "$c + $a / $b = $(( c + a / b ))"
+echo "$a % $b + $c = $(( a % b + c))"
+
